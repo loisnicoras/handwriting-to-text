ca5d6c0fbfd751d21d6bcb67dc42eaf706cf9288
loisnicoras <nicoraslois@gmail.com>
2024-02-22 13:54:37 +0200

    Add database schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `user` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `exercises` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `exercise_name` varchar(255) NOT NULL,
+  `audio_path` varchar(255) NOT NULL,
+  `text` varchar(255) NOT NULL
+);
+
+CREATE TABLE `users_results` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_id` integer NOT NULL,
+  `exercise_id` integer NOT NULL,
+  `photo_text` varchar(255) NOT NULL,
+  `generate_text` varchar(255) NOT NULL,
+  `result` integer
+);
+
+ALTER TABLE `users_results` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `users_results` ADD FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`);
