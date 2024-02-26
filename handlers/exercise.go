1342119d9f1a44e1cf525fd7505bccbffeed562b
Robert Oros <robertstefanoros@gmail.com>
2024-02-26 16:26:41 +0200

    Create logic for GetExercise handler

--- handlers/exercise.go
+++ handlers/exercise.go
@@ -4,17 +4,45 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type Exercise struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID            int    `json:"id"`
+	Name          string `json:"name"`
+	Audio_path    string `json:"audio_path"`
 }
 
 func GetExercise(w http.ResponseWriter, r *http.Request) {
+	db, err := connectToDB()
+	exerciseID := chi.URLParam(r, "exerciseID")
+
+	if err != nil {
+		http.Error(w, "failed to connect to the db", http.StatusInternalServerError)
+		return
+	}
+
+	var exercise Exercise
+
+	query := "SELECT * FROM your_table WHERE your_id = ?"
+	row := db.QueryRow(query, exerciseID)
+
+	err = row.Scan(&exercise.ID, &exercise.Name, &exercise.Audio_path)
+	if err != nil {
+		http.Error(w, "the row doesn't exist in db", http.StatusInternalServerError)
+		return
+	}
+
+	exerciseJSON, err := json.Marshal(exercise)
+	if err != nil {
+		http.Error(w, "failed to marshal the json", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	w.Write(exerciseJSON)
 }
 
 func GetExercises(w http.ResponseWriter, r *http.Request) {
