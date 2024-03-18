f36060055ca7d4f1606ba646528614f92cdd44f9
loisnicoras <nicoraslois@gmail.com>
2024-03-18 16:08:56 +0200

    Modify exercise

--- web/src/Components/Exercise.js
+++ web/src/Components/Exercise.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Exercise(props){
+    const [fetchedData, setFetchedData] = useState(null);
+
+    const getExercice = async (buttonId) => {
+        try {
+          // Perform the fetch request with the buttonId concatenated to the URL
+          const response = await fetch(`http://localhost:8080/exercises/${buttonId}`);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          setFetchedData(data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
     return (
         <div>
-            <button><span className="label">{props.data.name}</span></button>
+            <button onClick={() => getExercice(props.data.id)}>{props.data.name}</button>
         </div>
     );
 }
