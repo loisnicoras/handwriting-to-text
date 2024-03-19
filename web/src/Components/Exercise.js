9925f380ce6593b885348f4af6e1107f0e6497d3
loisnicoras <nicoraslois@gmail.com>
2024-03-19 13:49:27 +0200

    Add a paragraph in exercise component

--- web/src/Components/Exercise.js
+++ web/src/Components/Exercise.js
@@ -4,6 +4,7 @@ function Exercise(props) {
 
     return(
         <div>
+            <p>{props.data.name}</p>
             <audio controls src={props.data.audio_path}>
                 Your browser does not support the
                 <code>audio</code> element.
@@ -11,12 +12,12 @@ function Exercise(props) {
             <br />
             <input type="file" id="takePhoto" accept="image/*" capture="camera" style={{ display: 'none' }} />
             <button onClick={() => document.getElementById('takePhoto').click()}>
-            Take a picture
+                Take a picture
             </button>
             <br />
             <input type="file" id="uploadPhoto" accept="image/*" style={{ display: 'none' }} />
             <button onClick={() => document.getElementById('uploadPhoto').click()}>
-            Upload a picture
+                Upload a picture
             </button>
             <br />
         </div>
