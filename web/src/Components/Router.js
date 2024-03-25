5688724064061f91e08e9a947dc363e57e120052
Robert Oros <robertstefanoros@gmail.com>
2024-03-25 15:00:18 +0200

    Add router

--- web/src/Components/Router.js
+++ web/src/Components/Router.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import {
-    BrowserRouter,
-    Switch,
-    Route,
-    Link,
-    Routes
-  } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 import App from './App';
-import ExerciseName from './ExerciseName';
-
+import { BrowserRouter } from "react-router-dom";
 
 function AppRouter() {
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}>
-            </Route>
-            <Route path="/exercise" element={<ExerciseName/>}>
-            </Route>
-        </Routes>
-    </BrowserRouter>   
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <App/> } />
+            {/* <Route path="about" element={ <Users/> } /> */}
+          </Routes>
+        </BrowserRouter>
+    )
 }
+  
 
 export default AppRouter;
--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-import App from './Components/App';
+// import App from './Components/App';
 import Navbar from './Components/Navbar';
 // import reportWebVitals from './reportWebVitals';
-
+// import { RouterProvider } from "react-router-dom";
+import AppRouter from './Components/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
-    <App />
+    <AppRouter />
   </React.StrictMode>
 );
 
