09354edf298458308147a04e465b051126070a05
Robert Oros <robertstefanoros@gmail.com>
2024-04-02 13:26:30 +0300

    Modify

--- web/src/Components/SingleExercise.js
+++ web/src/Components/SingleExercise.js
@@ -1,57 +1,49 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, Form } from "react-router-dom";
 
 const SingleExercise = () => {
     const { id } = useParams();
     const [exercise, setExercise] = useState([]);
-    const [getText, setGetText] = useState({})
+    const [genText, setGenText] = useState("");
     const [file, setFile] = useState(null);
 
-    const handleFileChange = (event) => {
+    const handleFileChange = async (event) => {
         setFile(event.target.files[0]);
-    };
-
-    const handleChange = (event) => {
-        setGetText(event.target.value);
-    };
-
-    const obtainText = async () =>{
         const formData = new FormData();
-        formData.append('file', file);
+        formData.append('file', event.target.files[0]);
         try {
-            // Perform the fetch request with the buttonId concatenated to the URL
             const response = await fetch(`http://localhost:8080/extract-text`, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
                 body: formData
             });
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            const data = await response.json();
-            setGetText(data);
-            } catch (error) {
+            const data = await response.text(); // assuming the response is plain text
+            console.log(response)
+            setGenText(data);
+        } catch (error) {
             console.error('Error fetching data:', error);
         }
-    }
+    };
+
+    const handleChange = (event) => {
+        setGenText(event.target.value);
+    };
 
     useEffect(() => {
         const getExercice = async () => {
             try {
-                // Perform the fetch request with the buttonId concatenated to the URL
                 const response = await fetch(`http://localhost:8080/exercises/${id}`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
                 setExercise(data);
-                console.log(exercise)
-                } catch (error) {
+            } catch (error) {
                 console.error('Error fetching data:', error);
             }
-            };
+        };
         getExercice();
         return () => setExercise(null);
     }, [id]);
@@ -62,23 +54,15 @@ const SingleExercise = () => {
 
     return (
         <div>
-            {<Link to={`/`}>back</Link>}
+            <Link to={`/`}>back</Link>
             <p>{exercise.name}</p>
             <audio controls src={exercise.audio_path}>
                 Your browser does not support the
                 <code>audio</code> element.
             </audio>
-            <form onSubmit={obtainText}>
-                <br />
-                <input type="file" onChange={handleFileChange} accept="image/*" capture="camera" />
-                <br />
-                <input type="file" onChange={handleFileChange} accept="image/*" />
-                <br />
-                <input type="submit" value="Upload" />
-            </form>
-            {(getText != {}) && (
-                <textarea value={getText} onChange={handleChange} rows={4} cols={50} />
-            )}
+            <input type="file" name="image" onChange={handleFileChange} accept="image/*" />
+            <br />
+            <textarea value={genText} onChange={handleChange} rows={4} cols={50} />
         </div>
     )    
 }
