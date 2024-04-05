450fb770f8bf01693631b3132bed250ff9e8e6fb
Robert Oros <robertstefanoros@gmail.com>
2024-04-05 17:11:09 +0300

    Modify Navbar

--- web/src/Components/Navbar.js
+++ web/src/Components/Navbar.js
@@ -4,50 +4,49 @@ import '../css/Navbar.css';
 function Navbar() {
     const [userName, setUserName] = useState("");
     const [userAvatarUrl, setUserAvatarUrl] = useState("");
-    const [isLogged, setIsLogged] = useState(false)
+    const [isLogged, setIsLogged] = useState(false);
 
     useEffect(() => {
         (async () => {
             try {
-
                 const res = await fetch("http://localhost:8080/user-data", {
                     method: "GET",
                     credentials: "include"
-                })
+                });
 
-                if (res.status == 200) {
-                    setIsLogged(true)
-                } else if (res.status == 402) {
-                    setIsLogged(false)
+                if (res.status === 200) {
+                    setIsLogged(true);
+                    const result = await res.json();
+                    
+                    setUserName(result.name);
+                    setUserAvatarUrl(result.avatar_url);
+                } else if (res.status === 401) {
+                    setIsLogged(false);
                 } else {
                     throw new Error("Failed to fetch user data");
                 }
-                
-                const result = await res.json()
-                setUserName(result.name)
-                setUserAvatarUrl(result.avatar_url)
             } catch (error) {
                 console.error("Error fetching user data:", error);
-            }    
+            }
         })();
-    })
-    
-    if (isLogged) {
+    }, []);
+
+    if (!isLogged) {
         return (
             <nav>
-                <p>{userName}</p>
-                <img src={userAvatarUrl} alt="Logo"/>
-                <br />
-                <a href="http://localhost:8080/logout">logout</a>
+                <a href="http://localhost:8080/login">Login</a>
             </nav>
         );
-    } else {
-        <nav>
-            <a href="http://localhost:8080/login">login</a>
-        </nav>
     }
 
-   
+    return (
+        <nav>
+            <p>{userName}</p>
+            <img src={userAvatarUrl} alt="Logo" />
+            <br />
+            <a href="http://localhost:8080/logout">Logout</a>
+        </nav>
+    );
 }
 
 export default Navbar;
