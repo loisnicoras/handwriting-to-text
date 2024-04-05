52055822b3693cce824e309fc3f8192c19f52bdb
Robert Oros <robertstefanoros@gmail.com>
2024-04-05 16:10:46 +0300

    Modify in Navbar

--- web/src/Components/Navbar.js
+++ web/src/Components/Navbar.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../css/Navbar.css';
 
 function Navbar() {
+    const [userAvatarUrl, setUserAvatarUrl] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:8080/avatar", {
+            method: "GET",
+            credentials: "include"
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            setUserAvatarUrl(data.AvatarURL)
+        })
+    
+    })
+
     return (
         <nav>
             <a href="http://localhost:8080/login">login</a>
