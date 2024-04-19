53ad4928f7e3169634f66514dd24135f9faedc81
loisnicoras <nicoraslois@gmail.com>
2024-04-19 12:16:34 +0300

    feat(web): add ExerciseTypes and corresponding route

---
+++ web/src/Components/ExerciseTypes.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function ExerciseTypes(){
+    return (
+        <Link to="/exercises/audio-exercises/">Audio exercise</Link>
+    )
+}
+
+export default ExerciseTypes;
--- web/src/Components/Router.js
+++ web/src/Components/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ExerciseList from './ExerciseList';
 import SingleExercise from "./SingleExercise";
+import ExerciseTypes from "./ExerciseTypes";
 
 function AppRouter() {
     return (
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={ <ExerciseTypes/> } />
             <Route path="/exercises/audio-exercises/" element={ <ExerciseList/> } />
             <Route path="/exercises/audio-exercises/:id" element={ <SingleExercise/> } />
           </Routes>
@@ -14,23 +16,3 @@ function AppRouter() {
 }
   
 export default AppRouter;
-
-
-
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-
-// const SingleExercise = () => {
-//   // Extracting the id parameter from the route
-//   const { id } = useParams();
-
-//   return (
-//     <div>
-//       <h2>Exercise Details</h2>
-//       <p>Exercise ID: {id}</p>
-//       {/* Other details of the exercise can be displayed here */}
-//     </div>
-//   );
-// };
-
-// export default SingleExercise;
